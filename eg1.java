3baabf0e8b536f2d82e5510c72b35fbe6c08a5f0
Khushi <shrivastavak057@gmail.com>
2023-03-20 21:37:01 +0530

    Third Commit

--- eg1.java
+++ eg1.java
@@ -3,6 +3,7 @@ class eg1psp
     public static void main(String gg[])
     {
         System.out.println("Hello");
-        System.out.println("Khushi is a good girl");
+       System.out.println("Khushi is a good girl");
+       int x=10;
     }
-}
+ˇ}ˇ
