d8c406f2351d95223e43b5ead4bca7e35c607794
Khushi <shrivastavak057@gmail.com>
2023-03-20 23:00:00 +0530

    Khushi loves dancing

--- eg1.java
+++ eg1.java
@@ -3,7 +3,7 @@ class eg1psp
     public static void main(String gg[])
     {
         System.out.println("Hello");
-        System.out.println("Khushi is a good girl");
+        System.out.println("Khushi is a good girl and loves dancing");
         System.out.println("Khushi loves Zurich,Switzerland");
     }
 }
